fb45afd74140b9743637fa9020541a12da0619a3
NerySonic6 <95397031+NerySonic6@users.noreply.github.com>
2021-12-02 08:25:22 -0800

    Update CreditsState.hx

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -46,8 +46,8 @@ class CreditsState extends MusicBeatState
 		add(grpOptions);
 
 		var pisspoop = [ //Name - Icon name - Description - Link - BG Color
-			['Android Port'],
-			['M.A. Jigsaw',		    'majigsaw',		    'Main Coder of The Port',	 'https://www.youtube.com/channel/UC2Sk7vtPzOvbVzdVTWrribQ',	'0xFFC30085'],
+			['fnf vs glitch Spongebob Port'],
+			['NerySonic',		    'nerysonic',		    'Fnf vs glitch Spongebob The Port',	 'https://youtube.com/channel/UC-22MauRVLz9U7QI-BDB9fQ',	'0xFF6475F3']
 			['Psych Engine Team'],
 			['Shadow Mario',		'shadowmario',		'Main Programmer of Psych Engine',					'https://twitter.com/Shadow_Mario_',	'0xFFFFDD33'],
 			['RiverOaken',			'riveroaken',		'Main Artist/Animator of Psych Engine',				'https://twitter.com/river_oaken',		'0xFFC30085'],
