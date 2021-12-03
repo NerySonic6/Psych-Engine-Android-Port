abc40163839a881f8024f6db186d43e756edca37
NerySonic6 <95397031+NerySonic6@users.noreply.github.com>
2021-12-02 17:28:46 -0800

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -910,7 +910,7 @@ class PlayState extends MusicBeatState
 		add(scoreTxt);
 
 		creditTxt = new FlxText(876, 620, 348);
-        creditTxt.text = 'Ported By\Nerysonic';
+        creditTxt.text = 'Ported By\Nerysonic ';
         creditTxt.setFormat(Paths.font("vcr.ttf"), 30, FlxColor.WHITE, RIGHT);
         creditTxt.setBorderStyle(OUTLINE, FlxColor.BLACK, 3, 1);       
         creditTxt.scrollFactor.set();
